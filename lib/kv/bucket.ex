a9b1d2c0349073d94de929f374517fcdd9cee89e
Matheus Cumpian <matheus.cumpian@mercadolivre.com>
2022-08-20 11:47:30 -0300

    initial commit

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,40 @@
+defmodule KV.Bucket do
+  use Agent
+
+
+  @spec start_link(opts :: list(keyword())) :: {:error, any} | {:ok, pid}
+  @doc """
+    Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+
+  @spec get(atom() | pid(), any) :: any
+  @doc """
+    Get a value from the `bucket` by `key`
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @spec put(atom() | pid(), any, any) :: :ok
+  @doc """
+    Puts the `value` for the given `key` in the `bucket`
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key , value))
+  end
+
+  @spec delete(atom() | pid(), any) :: any
+  @doc """
+    Deletes `key` from `bucket`
+
+    Returns the current `value` for `key` if `key` exists
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+
+end
---
+++ test/kv_test.exs
@@ -0,0 +1,27 @@
+defmodule KVTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+
+    %{bucket: bucket}
+  end
+
+
+  test "should store key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "should delete key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+end
